7304aaa894fe0f71c0a0c3c666e3d7bb127c931b
Thesoro <rob.whitehead@gmail.com>
2013-01-20 11:14:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robwnet::Application.config.secret_token = '4fa864aa912ba96088433ce09e18501ed5b3426c7cab515f1815dfeb79875164e47f175a1938da6804268f22bcb0bbc789685ade4a34663103927aabd73f3707'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Robwnet::Application.config.session_store :cookie_store, key: '_robwnet_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Robwnet::Application.config.session_store :active_record_store
